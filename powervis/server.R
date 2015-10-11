08db6866d72c31c5ba43729dc2252dfd203e754c
Dmitry Duplyakin <dmitry.duplyakin@colorado.edu>
2015-10-11 18:51:11 -0400

    Major improvements: averaging, selectable thresholds, error handling for cases with no data

--- powervis/server.R
+++ powervis/server.R
@@ -4,6 +4,9 @@ library('ggplot2')
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+  # Moving average function
+  ma <- function(z,n=input$AvgWindow){filter(z,rep(1/n,n), sides=1)}
+
   range_selection <- function(power, start, end) {
     # Convert Time strings to actual Time format 
     power$Time = as.POSIXct(power$TimeRAW)
@@ -18,6 +21,10 @@ shinyServer(function(input, output) {
 
     power_sel=power[power$Time>=t_sel_start & power$Time<=t_sel_end,]
     return(power_sel)
+  }
+
+  power_filter <- function(power, low, high) {
+    return (power[power$Power >= low & power$Power <= high,])
   }
   
   time_gaps <- function(times) {
@@ -36,12 +43,20 @@ shinyServer(function(input, output) {
 
     power_display = data.frame(power$Time, power$Power)
     print(summary(power_display))
+    cat("-----------------------\n")
 
-    susp_num=sum(power$Power<=10)
-    cat(paste("Number of suspicious samples (power<10): ", susp_num, "\n"))
-    if (susp_num > 0) {
-      cat("First 10 suspicious samples:\n")
-      h=head(power[power$Power<=10,], 10) 
+    susp_lo_num=sum(power$Power<input$LowThresh)
+    cat(paste("Number of suspicious \"low\" samples (power<", input$LowThresh, "): ", susp_lo_num, "\n"))
+    if (susp_lo_num > 0) {
+      cat("First 10 \"low\" samples (most likely, special event markers):\n")
+      h=head(power[power$Power<input$LowThresh,], 10) 
+      print(h[,c("Resource","TimeRAW","Power")])
+    }
+    susp_hi_num=sum(power$Power>input$HighThresh)
+    cat(paste("Number of suspicious \"high\" samples (power>", input$HighThresh, "): ", susp_hi_num, "\n"))
+    if (susp_hi_num > 0) {
+      cat("First 10 \"high\" samples:\n")
+      h=head(power[power$Power>input$HighThresh,], 10) 
       print(h[,c("Resource","TimeRAW","Power")])
     }
     
@@ -49,51 +64,90 @@ shinyServer(function(input, output) {
 
   output$analysis <- renderPrint({
     file=gsub(" ", "", paste("/var/log/power/",input$resource))
-    cat(paste("Stats for SELECTED power data in: ", file, "\n"))
     power_all = read.csv(file, col.names=c("Resource","TimeRAW","Power"))
-    power = range_selection(power_all, start=input$SelectionRange[1], end=input$SelectionRange[2])
+ 
+    if (nrow(power_all) > 0) {
+      # Select and prune
+      power = power_filter(range_selection(power_all, start=input$SelectionRange[1], end=input$SelectionRange[2]),input$LowThresh,input$HighThresh)
 
-    ot=power$Time[1]
-    cat(paste("The oldest timestamp: ", ot, "\n"))
-    lt=power$Time[length(power$Time)]
-    cat(paste("The latest timestamp: ", lt, "\n"))
-    N=length(power$Time)
-    cat(paste("Number of power measurements: ", N, "\n"))
-    cat(paste("Average interval between measurements (sec): ", sprintf("%.2f", difftime(lt,ot,units="secs")/N), "\n"))
+      # Number of samples needs to be at least as high as the size of the window for moving average 
+      if (nrow(power) >= input$AvgWindow) {
+        enough_data = TRUE
+
+        cat(paste("Stats for SELECTED power data in: ", file, "\n"))
+
+        # Adding a column with averaged power
+        power$PowerAVG = ma(power$Power) 
+        # Replacing initial NULL values with non-averaged values
+        power$PowerAVG[1:input$AvgWindow-1]=power$Power[1:input$AvgWindow-1]
+
+        ot=power$Time[1]
+        cat(paste("The oldest timestamp: ", ot, "\n"))
+        lt=power$Time[length(power$Time)]
+        cat(paste("The latest timestamp: ", lt, "\n"))
+        N=length(power$Time)
+        cat(paste("Number of power measurements: ", N, "\n"))
+        cat(paste("Average interval between measurements (sec): ", sprintf("%.2f", difftime(lt,ot,units="secs")/N), "\n"))
+
+        #cat("-----------------------\n")
+        #print(head(power))
+
+      }
+      else {
+        enough_data = FALSE
+        cat("Selected interval does not have enough samples in the allowed range.\nGraphs and statistics will not be shown.\n")
+      }
+    }
+    else {
+      enough_data = FALSE
+      cat("There appears to be no data for the chosen resource.")
+    }
+ 
+    # Saving selected and pruned power data frame so it can be loaded by other functions
+    # gsub deletes spaces present in input$resource
+    # Boolean enough_data is also saved to prevent errors (if it is false, graphs won't be shown)
+    save(enough_data,power, file = gsub(" ", "",paste("/tmp/",input$resource,"-power")))
   })
 
   output$Scatter <- renderPlot({
-    file=gsub(" ", "", paste("/var/log/power/",input$resource))
-    power_all = read.csv(file, col.names=c("Resource","TimeRAW","Power"))
-    power = range_selection(power_all, start=input$SelectionRange[1], end=input$SelectionRange[2])
+    load(gsub(" ", "",paste("/tmp/",input$resource,"-power")))
+    if (enough_data) {
+      df1=data.frame(Time=power$Time, Power=power$Power)
+      df2=data.frame(Time=power$Time, Power=power$PowerAVG)
     
-    qplot(power$Time, power$Power) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + geom_line() + geom_point()
+      #qplot(power$Time, power$Power) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + geom_smooth() + geom_point() 
+
+      ggplot(df1,aes(Time,Power))+geom_point(aes(color="Raw Samples"))+
+      geom_line(data=df2,aes(color="Moving Average"))+
+      labs(color="Series")+
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+      theme(legend.position="bottom")
+    }
   })
 
   output$HistPower <- renderPlot({
-    file=gsub(" ", "", paste("/var/log/power/",input$resource))
-    power_all = read.csv(file, col.names=c("Resource","TimeRAW","Power"))
-    power = range_selection(power_all, start=input$SelectionRange[1], end=input$SelectionRange[2])
-
-    qplot(power$Power,
-      geom="histogram",
-      main = "Histogram for Power",
-      xlab = "Power, Watts",
-      fill=I("blue"),
-      col=I("black"))
+    load(gsub(" ", "",paste("/tmp/",input$resource,"-power")))
+    if (enough_data) {
+      qplot(power$Power,
+        geom="histogram",
+        main = "Histogram for Power (Non-averaged)",
+        xlab = "Power, Watts",
+        fill=I("blue"),
+        col=I("black"))
+    }
   })
 
   output$HistGaps <- renderPlot({
-    file=gsub(" ", "", paste("/var/log/power/",input$resource))
-    power_all = read.csv(file, col.names=c("Resource","TimeRAW","Power"))
-    power = range_selection(power_all, start=input$SelectionRange[1], end=input$SelectionRange[2])
-    gaps = time_gaps(power$Time)
+    load(gsub(" ", "",paste("/tmp/",input$resource,"-power")))
+    if (enough_data) {
+      gaps = time_gaps(power$Time)
 
-    qplot(gaps,
-      geom="histogram",
-      main = "Histogram for Gaps between Measurements",
-      xlab = "Seconds",
-      fill=I("red"),
-      col=I("black"))
+      qplot(gaps,
+        geom="histogram",
+        main = "Histogram for Gaps between Measurements",
+        xlab = "Seconds",
+        fill=I("red"),
+        col=I("black"))
+    }
   })
 })
--- powervis/ui.R
+++ powervis/ui.R
@@ -20,9 +20,16 @@ shinyUI(pageWithSidebar(
     helpText("Note: all these files are found in /var/log/power"),
     hr(),
     sliderInput("SelectionRange", label = h4("Select boundaries for visualization"), min = 0,
-        max = 100, value = c(90, 100)),
-    helpText("Note: 100% corresponds to the latest record in the file, 0% - to the oldest")
+        max = 100, value = c(95, 100)),
+    helpText("Note: 100% corresponds to the latest record in the file, 0% - to the oldest"),
+    hr(),
+    sliderInput("LowThresh", label = h4("Low Power Threshold"), min = 0, max = 50, value = 10),
+    sliderInput("HighThresh", label = h4("High Power Threshold"), min = 100, max = 500, value = 300),
+    hr(),
+    sliderInput("AvgWindow", label = h4("Averaging Window"), min = 0, max = 10, value = 3)
+   
   ),
+
 
   mainPanel(
     verbatimTextOutput("summary"),
